ddafdca39bb5b9baa090fbcf5afc995cf8ea9c3f
Rémy Sanchez <remy.sanchez@hyperthese.net>
2011-08-23 04:28:45 +0200

    Oubli de translation.py

---
+++ translation.py
@@ -0,0 +1,20 @@
+########################################################################
+# vim: fileencoding=utf-8 ts=8 noexpandtab :
+#
+# ~~~~ Maiznet.fr ~~~~
+#
+#  -> translation.py
+#
+#
+# Copyright 2011 Rémy Sanchez <remy.sanchez@hyperthese.net>
+#
+# This file is distributed under the terms of the WTFPL. For more
+# informations, see http://sam.zoy.org/wtfpl/COPYING
+########################################################################
+from modeltranslation.translator import translator, TranslationOptions
+from maiznet.pages.models import Page
+
+class PageTranslationOptions(TranslationOptions):
+	fields = ('title', 'content')
+
+translator.register(Page, PageTranslationOptions)
